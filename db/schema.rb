a8ff00114fa55a3218032063f0307a2d1486282d
Gabriel Roux <rouxgab81@gmail.com>
2021-02-15 20:37:40 +0100

    Add phone number to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_192318) do
+ActiveRecord::Schema.define(version: 2021_02_15_192735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_192318) do
     t.string "username", null: false
     t.string "address", null: false
     t.text "presentation", null: false
+    t.integer "phone_number", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
