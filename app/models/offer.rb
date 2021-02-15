6be0625d56f7fef24e2991de0375b58cf349449e
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-02-15 17:44:33 +0100

    addin Active Strorage

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,7 @@
 class Offer < ApplicationRecord
   DISC = ["Volley", "Poker", "Belotte"]
   belongs_to :user
+  has_one_attached :photo
   validates :title, presences: true, length: { minimum: 50 }
   validates :description, presences: true
   validates :localisation, presences: true, length: { minimum: 50 }
