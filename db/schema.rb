6f87f35c46134ca32e1bd46e5148b0c5e53660e9
Gabriel Roux <rouxgab81@gmail.com>
2021-02-15 20:43:52 +0100

    Add birth date to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_192735) do
+ActiveRecord::Schema.define(version: 2021_02_15_193932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_192735) do
     t.string "address", null: false
     t.text "presentation", null: false
     t.integer "phone_number", null: false
+    t.date "birth_date", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
