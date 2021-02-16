574c88cdb113236dbe014cac1992930cf6b59003
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-02-16 22:05:02 +0100

    bugfix + tentative de liste + ajout a la show

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Offer < ApplicationRecord
   has_many :reviews
   belongs_to :user
@@ -9,8 +11,17 @@ class Offer < ApplicationRecord
   validates :localisation, presence: true, length: { maximum: 50 }
   validates :discipline, presence: true, inclusion: { in: ["Volley", "Poker", "Belotte"],
     message: "%{value} is not a valid discipline" }
+  # validates :discipline, presence: true, inclusion: { in: :sports_list,
+  #   message: "%{value} is not a valid discipline" }
 
-  # def self.sports
-  #   ["Volley", "Poker", "Belotte"]
-  # end
+
+  def self.sports_list
+  #   # ["Volley", "Poker", "Belotte"]
+    sport_array = []
+    CSV.foreach('app/assets/data/sportlist.csv') do |row|
+      sport_array << row
+    end
+    sport_array
+  end
+
 end
