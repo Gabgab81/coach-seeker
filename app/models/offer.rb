c88d1057c48ec385bc3c2d80ab4daf18524a1d71
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-02-16 15:54:16 +0100

    show ok

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,11 +1,16 @@
 class Offer < ApplicationRecord
-  DISC = ["Volley", "Poker", "Belotte"]
   has_many :reviews
   belongs_to :user
   has_one_attached :photo
+  has_rich_text :description
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true
   validates :localisation, presence: true, length: { maximum: 50 }
-  validates :discipline, presence: true, inclusion: { in: ["Volley", "Poker", "Belotte"],
+  validates :discipline, presence: true, inclusion: { in: sports,
     message: "%{value} is not a valid discipline" }
+
+  def self.sports
+    ["Volley", "Poker", "Belotte"]
+  end
 end
