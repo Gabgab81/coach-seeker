bc504ec9e8aa06010df6d1f8b70f7d5fdc9265f7
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-02-17 22:17:27 +0100

    add 5 next coaching to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user if user_signed_in?
+    @coachings = Coaching.where(user_id: current_user).last(5)
   end
 
-  
+
 
 end
